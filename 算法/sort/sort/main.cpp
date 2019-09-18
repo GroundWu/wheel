747ef379fa963e071c02000ed5369776d146649a
wgx <1175416256@qq.com>
2019-09-18 19:28:57 +0800

    quick sort

--- 算法/sort/sort/main.cpp
+++ 算法/sort/sort/main.cpp
@@ -1,12 +1,12 @@
 #include <iostream>
-#include "insert_sort.h"
+#include "inner_sort.h"
 using namespace std;
 
 int main()
 {
-    int A[] = {1,3,7,1,2,9,5,3,12,4,6};
+    int A[] = {1,3,2,4,6,9,7,4,8,11,10};
     int n = sizeof(A)/sizeof(int);
-
+    /*
     cout<<"origin"<<endl;
     for(int i=0;i<n;i++)
         cout<<A[i]<<" ";
@@ -25,6 +25,37 @@ int main()
     for(int i=0;i<n;i++)
         cout<<A[i]<<" ";
     cout<<endl;
+    */
 
+    cout<<"origin"<<endl;
+    for(int i=0;i<n;i++)
+        cout<<A[i]<<" ";
+    cout<<endl;
+    // bub sort
+    //bub_sort(A,n,true);
+    // select sort
+    //select_sort(A,n,true);
+    // shell sort
+    //shell_sort(A,n,true);
+    // merge sort
+    //merge_sort(A,n,true);
+    //quick sort
+    quick_sort(A,n,true);
+
+    cout<<"sorted(desc)"<<endl;
+    for(int i=0;i<n;i++)
+        cout<<A[i]<<" ";
+    cout<<endl;
+
+    //bub_sort(A,n,false);
+    //select_sort(A,n,false);
+    //shell_sort(A,n,false);
+    // merge_sort(A,n,false);
+    quick_sort(A,n,false);
+
+    cout<<"sorted(asc)"<<endl;
+    for(int i=0;i<n;i++)
+        cout<<A[i]<<" ";
+    cout<<endl;
     return 0;
 }
